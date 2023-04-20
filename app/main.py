4f41131fade47505ce02630b742db00db921c235
Jose Mosquera <jojumofu@gmail.com>
2023-04-20 10:08:24 -0500

    update: add wtf,methods and forms in app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'secret_key_here'
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -16,11 +28,23 @@ def no_server(error):
 @app.route('/')
 def index():
     return 'Hello, Pepe!'
+    
 
-@app.route('/dashboard')
+@app.route('/dashboard', methods=['GET', 'POST'])
 def login():
     #return 'Welcome tu login bots'
-    return render_template('dashboard.html')
+    login_form = LoginForm()
+
+    context = {'login_form': login_form}
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usario registrado con éxito!')
+
+        return redirect(url_for('index'))
+    
+    return render_template('dashboard.html', **context)
 
 if __name__ == '__main__':
     app.run()
