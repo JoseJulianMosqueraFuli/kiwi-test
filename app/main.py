e06c43589e2167c97d48a5cbad8b2a3daaef833a
Jose Mosquera <jojumofu@gmail.com>
2023-04-20 09:43:45 -0500

    update: add boostrap and errorhandlers

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def no_server(error):
+  return render_template('500.html', error=error)  
+
 
 @app.route('/')
 def index():
